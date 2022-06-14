8b70d95f85d28fba463b447d9e48d957d9cf2d00
KostasDm <kostdam92@gmail.com>
2022-06-14 09:33:16 +0200

    add update analysis tools

--- SQC_analysis_tools.py
+++ SQC_analysis_tools.py
@@ -7,20 +7,26 @@ import warnings
 from statistics import mean, stdev, median
 import glob
 import yaml
+import traceback
+from scipy.stats import linregress
+import scipy.signal
+
 
 
 headers_Stripscan = ['Pad', 'Istrip', 'Rpoly', 'Cac', 'Cac_Rp', 'Idiel', 'Cint', 'Cint_Rp', 'Idark', 'Rint', 'Temperature','Humidity']
 
-headers_IVC = ['Voltage [V]',  'current [A]' , 'temperature [deg]', 'humidity [%]'] #'capacitance [F]',
+headers_IVC = ['Voltage [V]',  'current [A]' , 'capacitance [F]', 'temperature [deg]', 'humidity [%]'] 
 
 headers_HPK = ['Voltage [V]',  'current [A]']
 
 headers_HPK_cv = ['Voltage [V]',  'capacitance [F]']
 
 
+
+
 def read_config():
     with open('SQC_parameters.yml', 'r') as f:
-        conf = yaml.safe_load(f)#, Loader=yaml.FullLoader)
+        conf = yaml.load(f, Loader=yaml.FullLoader)
 
     return conf
 
@@ -36,6 +42,8 @@ def get_parameter(filename, parameter, clear):
     dict = make_dictionary(parameter, filename, sensor_id)
 
     return dict
+
+
 
 def make_dictionary(main_parameter, file, prefix):
 
@@ -105,65 +113,28 @@ def convert_txt_to_df(filename, headers, skip):
 
     return df
 
-def make_Dataframe_Stripscan(parameter, filename, sensor_id, clear=False):
+
+def make_Dataframe_Stripscan(parameter, filename, sensor_id):
 
 
-    if clear:
-       a= convert_txt_to_df(filename, headers_Stripscan, 23 if sensor_id=='2-S' else 16) #16
-       a = a[a[parameter].notnull()]
-       par = a[parameter]
-       pad = a['Pad']
+    
+    df= convert_txt_to_df(filename, headers_Stripscan, 23 if sensor_id=='2-S' else 16) #16
+    df[parameter] = pd.to_numeric(df[parameter])
+    df = df.dropna(subset=[parameter])
 
-    else:
-       df = convert_txt_to_df(filename, headers_Stripscan, 23 if sensor_id=='2-S' else 16) #16
-
-    if parameter is '':
-        return df
-    else:
-        return pad, par
+    return df
 
 
 
-def make_Dataframe_IVCV(parameter, filename, clear=False):
+def make_Dataframe_IVCV(filename, start_line):
 
-    if clear:
-        a = convert_txt_to_df(filename, headers_IVC, 9) #9 23 77
-        df = a.dropna()
-    else:
-        df = convert_txt_to_df(filename, headers_IVC, 9)#9
+    
+    df = convert_txt_to_df(filename, headers_IVC, start_line) 
+    df = df.dropna()
+    
 
-    if parameter is '':
-        return df
-    else:
-        return df[parameter]
-
-
-
-def make_Dataframe_HPK_IV(parameter, filename, clear=False):
-
-    if clear:
-        a = convert_txt_to_df(filename, headers_HPK, 23)
-        df = a.dropna()
-    else:
-        df = convert_txt_to_df(filename, headers_HPK, 23)
-
-    if parameter is '':
-        return df
-    else:
-        return df[parameter]
-
-def make_Dataframe_HPK_CV(parameter, filename, clear=False):
-
-    if clear:
-        a = convert_txt_to_df(filename, headers_HPK_cv, 76)
-        df = a.dropna()
-    else:
-        df = convert_txt_to_df(filename, headers_HPK_cv, 76)
-
-    if parameter is '':
-        return df
-    else:
-        return df[parameter]
+    return df
+   
 
 
 
@@ -173,53 +144,32 @@ def MAD(parameter, parameter_median):
     return parameter_mad
 
 
-def numpy_MAD(parameter, parameter_median):
-
-    parameter_mad = np.median(np.abs(parameter - parameter_median))
-    return parameter_mad
-
-
 
 def assign_label(file):
 
     file = os.path.splitext(file)[0]
-
-    lbl = '_'.join(file.split('_')[1:6])
-    batch = '_'.join(file.split('_')[1:2])
+    print(file)
+    lbl = '_'.join(file.split('_')[2:6])
+    batch = '_'.join(lbl.split('_')[0:1])
+    print(batch)
+  
     return lbl, batch
 
-'''
-def convert_txt_to_df(filename, parameter, skip, HPK_file):
 
-    if HPK_file:
-        dat = np.genfromtxt(filename, skip_header=skip, max_rows=51) #51
-        print(dat)
-    else:
-         dat = np.genfromtxt(filename, skip_header= skip)
-
-    if parameter in headers_Stripscan or parameter =='':
-           print(dat)
-           df = pd.DataFrame(dat, columns = headers_Stripscan)
-    else:
-           df = pd.DataFrame(dat, columns=headers_HPK if HPK_file else headers_IVC)
-
-    return df
-'''
 
 def plot_graph(x, y, color, label, title, xlab, ylab):
 
+     
      plt.plot(x, y, '-o', color=color, markersize=4, label =label)
      plt.title(title, fontname="Times New Roman", fontsize=16, fontweight='bold')
      plt.xlabel(xlab, fontsize=12)
      plt.ylabel(ylab, fontsize=12)
-     #v_dep1, v_dep2, a_rise, b_rise, v_rise, a_const, b_const, v_const, spl_dev = analyse_cv(x, 1./np.sqrt(y))
-     #plt.vlines(v_dep2, 0, 5.5e17, colors= 'black', linestyles = 'dashed')
-     #print(v_dep2)
-     #plt.annotate('Threshold of non-leaky strip', (0.5, 0.45), xycoords='figure fraction', color='black')
-     #plt.yscale('log')
+   
      plt.tick_params(axis = 'y', labelsize=10)
      plt.tick_params(axis='x', labelsize=10)
-     plt.legend(loc='best', fontsize=8, ncol=2)
+     
+     plt.legend(loc='best', fontsize=8, ncol=1)
+
 
 
 
@@ -230,7 +180,8 @@ def plot_scatter(x, y, color, label, title, xlab, ylab):
      plt.xlabel(xlab)
      plt.ylabel(ylab)
      #plt.yscale('log')
-     plt.legend()
+     #plt.legend()
+
 
 
 def plot_lines(x, y, color, label, title, xlab, ylab):
@@ -239,6 +190,7 @@ def plot_lines(x, y, color, label, title, xlab, ylab):
     plt.title(title)
     plt.xlabel(xlab)
     plt.ylabel(ylab)
+
 
 
 
@@ -256,78 +208,22 @@ def plot_histogram(x, title, xlab, color, lbl):
 
 
 
-def find_right_quantile(x):
-
-     q = np.quantile(x, 0.99)
-     return q
-
-
-def find_left_quantile(x):
-
-    v = np.quantile(x, 0.01)
-    return v
-
-
-def find_left_outliers(x):
-
-    l_quant = find_left_quantile(x)
-    r_out = [i for i in x if i<l_quant]
-    return r_out
-
-
-def find_right_outliers(x, r_quant):
-
-    #r_quant = find_right_quantile(x)
-    r_out = [i for i in x if i>r_quant]
-    return r_out
-
-
-def write_outliers(x):
-
-    a = []
-    l_quant = find_left_quantile(x)
-    r_quant = find_right_quantile(x)
-    a.append(x[x< l_quant])
-    a.append(x[x > r_quant])
-
-    f = open("outliers.txt", "a")
-    for i in a:
-      print(i)
-      f.write(str(i))
-    f.close()
 
 
 
-def reject_outliers(x):
 
-    #Z_left = find_left_quantile(x)
-    #Z_right = find_right_quantile(x)
-
-    x_filtered= []
-    x_mean = float(np.median(x))
-    print(type(x_mean))
-    for i in x:
-        if i<x_mean :
-            x_filtered.append(i)
-            #print(i)
-    print(x_filtered)
-
-    #x_filtered = x[~(x < Z_right) & (x > Z_left)]
-
-    return x_filtered
-
-
-
-#@params('v_dep1, v_dep2, rho, conc, a_rise, b_rise, v_rise, a_const, b_const, v_const, spl_dev, status')
-def analyse_cv(v, c, area=1.56e-6, carrier='electrons', cut_param=0.004, debug=False):
+def analyse_cv( v, c, area=1.56e-4, carrier='electrons', cut_param=0.008, max_v=500, savgol_windowsize=None, min_correl=0.1, debug=False):
     """
     Diode CV: Extract depletion voltage and resistivity.
     Parameters:
     v ... voltage
     c ... capacitance
-    area ... implant size in [m^2]
+    area ... implant size in [m^2] - defaults to quarter
     carrier ... majority charge carriers ['holes', 'electrons']
     cut_param ... used to cut on 1st derivative to id voltage regions
+    max_v ... for definition of fit region, only consider voltages < max_v
+    savgol_windowsize ... number of points to calculate the derivative, needs to be odd
+    min_correl ... minimum correlation coefficient to say that it worked
     Returns:
     v_dep1 ... full depletion voltage via inflection
     v_dep2 ... full depletion voltage via intersection
@@ -336,58 +232,77 @@ def analyse_cv(v, c, area=1.56e-6, carrier='electrons', cut_param=0.004, debug=F
     """
 
     # init
-    v_dep1 = v_dep2 = rho = conc = -1
-    a_rise = b_rise = v_rise = a_const = b_const = v_const = -1
-    #status = STATUS_NONE
+    v_dep1 = v_dep2 = rho = conc = np.nan
+    a_rise = b_rise = a_const = b_const = np.nan
+    v_rise = []
+    v_const = []
+    status = 'Pass'
+
+    if savgol_windowsize is None:
+        # savgol_windowsize = int(len(c) / 40 + 1) * 2 + 1  # a suitable off windowsie - making 20 windows along the whole measurement
+        savgol_windowsize = int(len(c) / 30 + 1) * 2 + 1  # a suitable off windowsie - making 15 windows along the whole measurement
+        # the window size needs to be an odd number, therefore this strange calculation
 
     # invert and square
-    #c = 1./c**2
+    #c = [1./i**2 for i in c]
 
     # get spline fit, requires strictlty increasing array
     y_norm = c / np.max(c)
     x_norm = np.arange(len(y_norm))
-    spl = CubicSpline(x_norm, y_norm)
-    spl_dev = spl(x_norm, 1)
+   
+    spl_dev = scipy.signal.savgol_filter(y_norm, window_length=savgol_windowsize, polyorder=1, deriv=1)
 
+    # for definition of fit region, only consider voltages < max_v
+    idv_max = max([i for i, a in enumerate(v) if abs(a) < max_v])
+    spl_dev = spl_dev[:idv_max]
 
-    # get regions for indexing
-    idx_rise = [ i for i in range(len(spl_dev)) if (abs(spl_dev[i]) > cut_param) ]
-    idx_const = [ i for i in range(len(spl_dev)) if (abs(spl_dev[i]) < cut_param) ]
+    idx_rise = []
+    idx_const = []
 
     with warnings.catch_warnings():
         warnings.filterwarnings('error')
 
         try:
-            v_rise = v[ idx_rise[-6]:idx_rise[-1]+1 ]
-            v_const = v[ idx_const[1]:idx_const[-1]+1 ]
-            c_rise = c[ idx_rise[-6]:idx_rise[-1]+1 ]
-            c_const = c[ idx_const[1]:idx_const[-1]+1 ]
+            # get regions for indexing
+            idx_rise = [i for i in range(2, len(spl_dev-1)) if ((spl_dev[i]) > cut_param)]  # the first and last value seems to be off sometimes
+            idx_const = [i for i in range(2, len(spl_dev-1)) if ((spl_dev[i]) < cut_param) and i > idx_rise[-1]]
+
+            v_rise = v[idx_rise[0]:idx_rise[-1] + 1]
+            v_const = v[idx_const[0]:idx_const[-1] + 1]
+            c_rise = c[idx_rise[0]:idx_rise[-1] + 1]
+            c_const = c[idx_const[0]:idx_const[-1] + 1]
 
             # line fits to each region
-            a_rise, b_rise = np.polyfit(v_rise, c_rise, 1)
-            a_const, b_const = np.polyfit(v_const, c_const, 1)
+            a_rise, b_rise, r_value_rise, p_value_rise, std_err_rise = scipy.stats.linregress(v_rise, c_rise)
+            a_const, b_const, r_value_const, p_value_const, std_err_const = scipy.stats.linregress(v_const, c_const)
 
+        
+            mu = 1350*1e-4 
 
             # full depletion voltage via max. 1st derivative
             v_dep1 = v[np.argmax(spl_dev)]
 
             # full depletion via intersection
             v_dep2 = (b_const - b_rise) / (a_rise - a_const)
+            
+            conc = 2. / (1.6e-19 * 11.68 *8.854e-12 * a_rise * area**2) 
+            rho = 1. / (mu*1.6e-19 *conc)
 
-            # rest
 
-            #status = STATUS_PASSED
 
         except np.RankWarning:
-            #status = STATUS_FAILED
+            
             print("The array has too few data points. Try changing the cut_param parameter.")
 
         except (ValueError, TypeError, IndexError):
-            #status = STATUS_FAILED
+        
             print("The array seems empty. Try changing the cut_param parameter.")
 
-    return v_dep1, v_dep2, a_rise, b_rise, v_rise, a_const, b_const, v_const, spl_dev#, status
-
+        if status == 'Fail':
+            #print("The fit didn't work as expected, returning nan")
+            return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, STATUS_FAILED
+    
+    return v_dep1, v_dep2, rho
 
 
 
@@ -420,6 +335,7 @@ def normalise_parameter(parameter, unit):
     denominator, unit = units(parameter, unit)
     #x = np.array([j / denominator for j in parameter])
     x = parameter.divide(denominator)
+    
     return x, unit
 
 
