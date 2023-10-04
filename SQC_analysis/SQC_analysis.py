6b9cb2131fb99dd0ca9214035b0d6b978b0a79ed
Kostas Damanakis <55495267+KostasDm@users.noreply.github.com>
2023-10-04 23:04:16 +0200

    update SQC_data_analysis script

--- SQC_analysis/SQC_analysis.py
+++ SQC_analysis/SQC_analysis.py
@@ -29,9 +29,8 @@ def IV_analysis(file):
      df = sqc.make_Dataframe_IVCV(file, start_line)
      voltage = df['Voltage [V]'].abs()
      current = df['current [A]'].abs()
-     
-     
-     
+    
+         
      return voltage, current
 
 
@@ -75,20 +74,20 @@ def stripscan_analysis(filename, parameter):
   
   
 
-def overlay_plots(sensor_of_interest, files, key, values):
+def overlay_plots(files, key, values):
 
     # itterates over all input files and produces the plots.
     # key-values refer to the dictionary produced from the configuration file
-    # sensor_of_interest is the single tested sensor in SQC, important to evaluate whether its parameters are within the expected range
-    
+        
     color_index =-1
     flag = True
    
     for file in files:
       
        if not "HPK" in file:
-          lbl='-'.join(os.path.basename(file).split('-')[:1 if 'PSS' in file else 2])
-
+           lbl='-'.join(os.path.basename(file).split('-')[:1 if 'PSS' in file else 2])
+           lbl='_'.join(lbl.split('_')[1:])
+           lbl=lbl.split('.')[0]
        else:
           lbl = os.path.basename(file)  
     
@@ -106,18 +105,14 @@ def overlay_plots(sensor_of_interest, files, key, values):
        y = y*(float(values['units_conversion']))
        color_index +=1
 
-       sqc.plot_graph(abs(x), abs(y), color[color_index], lbl, values['title'], values['variables'][0], values['variables'][1] + ' ' +'[{}]'.format(values['units']))
+       sqc.plot_graph(abs(x), abs(y), color[color_index], lbl, values['title'], values['variables'][0] + ' ' , values['variables'][1] + ' ' +'[{}]'.format(values['units']))
         
        
-       if (y.abs()).max() > (100*y.abs()).median() and "Str" in file:
+       if (y.abs()).max() > (50*y.abs()).median() and "Str" in file:
          
           plt.yscale('log')
           
-       
-       if sensor_of_interest in file:
-           flag = sqc.evaluate_results(y, values, key, '2-S' if '2-S' in file else 'PSS')
-           
-    return flag
+    
        
      
 def find_most_recent_file(files):
@@ -138,11 +133,10 @@ def find_most_recent_file(files):
 
 
 
-def make_pdf_with_plots(sensor, files, measurement_config, pdf_name):
+def make_pdf_with_plots(files, measurement_config, pdf_name):
 
   # produces the pdfs with the plots
   
-  dictionary_with_flags = {}
   
   with PdfPages('{}.pdf'.format(pdf_name)) as pdf:
       for key,values in measurement_config.items():
@@ -150,12 +144,10 @@ def make_pdf_with_plots(sensor, files, measurement_config, pdf_name):
          scale_factor = values['units_conversion'] # to scale the parameter to the typical order of magnitude
          
          fig = plt.figure()
-         flag = overlay_plots(sensor, files, key, values) 
+         overlay_plots(files, key, values) 
          pdf.savefig(fig)
-         dictionary_with_flags[key] = flag
+         
 
-
-  return dictionary_with_flags
   
   
 def build_slack_message(dictionary, sensor):
@@ -179,7 +171,7 @@ def parse_args():
 
     parser = argparse.ArgumentParser(epilog = 'a path to directory containing ascii files to be analysed')
     parser.add_argument('path')
-    parser.add_argument('sensor')
+   # parser.add_argument('sensor')
     return parser.parse_args()
 
 
@@ -190,7 +182,7 @@ def main():
 
     args = parse_args()
 
-    filename = glob.glob(f'{args.path}/*/*.txt', recursive=True)
+    filename = glob.glob(f'{args.path}/*/*.txt', recursive=True) 
     
     most_recent_files = find_most_recent_file(filename)
    
@@ -205,14 +197,14 @@ def main():
     Stripscan_config = sqc.read_config()['Strip_Parameters']
     
     if len(IVCV_files)>=1:
-        dictionary_witf_flags = make_pdf_with_plots(args.sensor, IVCV_files, IV_config, "IVCV")
+         make_pdf_with_plots(IVCV_files, IV_config, "IVCV")
         
     if len(Stripscan_files)>=1:
-        dictionary_with_flags = make_pdf_with_plots(args.sensor, Stripscan_files, Stripscan_config, "Stripscan")
+         make_pdf_with_plots(Stripscan_files, Stripscan_config, "Stripscan")
     
     
-    message = build_slack_message(dictionary_with_flags, args.sensor)
-    sqc.send_slack_message(message)
+    #message = build_slack_message(dictionary_with_flags, args.sensor)
+   # sqc.send_slack_message(message)
     
    
 
